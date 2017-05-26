6e6370364f0016a3905443d018a51999f69b67dd
renjp <jinping.ren@nanosic.com>
2017-05-26 16:25:01 +0800

    branchone ver1.02

--- hal.c
+++ hal.c
@@ -1,6 +1,7 @@
 /*
  * 2017-05-26 V1.00
  * V1.01
+ * branchOne V1.02
  *
  */
 int halInit(int val)
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /*
  *  2017-05-26  Ver1.01
  *  add version V1.01
+ *  branchOne V1.02
  *
  */
 void main()
