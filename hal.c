13f3d484485540ad4845d99b5d8d0f4f0f45ba8b
renjp <jinping.ren@nanosic.com>
2017-05-26 17:20:06 +0800

    main branch add

--- hal.c
+++ hal.c
@@ -2,6 +2,7 @@
  * 2017-05-26 V1.00
  * V1.01
  *
+ *  mast branch : add val
  */
 int halInit(int val)
 {
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
  *  2017-05-26  Ver1.01
  *  add version V1.01
  *
+ *  mast: add val
  */
 void main()
 {
